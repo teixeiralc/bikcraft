2ecb3c160fcefc525865d7e09be3d4ff194ea9b3
Lucas Teixeira <lucascteixeira0@gmail.com>
2022-10-25 11:08:57 -0300

    add mobile hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Activate menu links
 
-const links = document.querySelectorAll(".header-menu a");
+const links = document.querySelectorAll('.header-menu a');
 
 function activeLink(link) {
   const url = location.href;
   const href = link.href;
 
   if (url.includes(href)) {
-    link.classList.add("active");
+    link.classList.add('active');
   }
 }
 
@@ -28,39 +28,39 @@ params.forEach(activateProduct);
 
 // Faq
 
-const faq = document.querySelectorAll(".faq button");
+const faq = document.querySelectorAll('.faq button');
 
 function activateQuestion(event) {
   const question = event.currentTarget;
-  const controls = question.getAttribute("aria-controls");
+  const controls = question.getAttribute('aria-controls');
   const answer = document.getElementById(controls);
 
-  answer.classList.toggle("active");
-  const active = answer.classList.contains("active");
-  question.setAttribute("aria-expanded", active);
+  answer.classList.toggle('active');
+  const active = answer.classList.contains('active');
+  question.setAttribute('aria-expanded', active);
 }
 
 function faqEvents(question) {
-  question.addEventListener("click", activateQuestion);
+  question.addEventListener('click', activateQuestion);
 }
 
 faq.forEach(faqEvents);
 
 // bicycles gallery
 
-const gallery = document.querySelectorAll(".bike-img img");
-const container = document.querySelector(".bike-img");
+const gallery = document.querySelectorAll('.bike-img img');
+const container = document.querySelector('.bike-img');
 
 function changeImage(event) {
   const img = event.currentTarget;
-  matchMedia("(min-width: 1000px)").matches;
-  if (matchMedia("(min-width: 1000px)").matches) {
+  matchMedia('(min-width: 1000px)').matches;
+  if (matchMedia('(min-width: 1000px)').matches) {
     container.prepend(img);
   }
 }
 
 function galleryEvent(img) {
-  img.addEventListener("click", changeImage);
+  img.addEventListener('click', changeImage);
 }
 
 gallery.forEach(galleryEvent);
@@ -70,3 +70,29 @@ gallery.forEach(galleryEvent);
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+// Mobile Menu
+
+import outsideClick from './plugins/outside-click.js';
+
+const userEvents = ['touchstart', 'click'];
+const mobileBtn = document.querySelector('.mobile-btn');
+const menuList = document.querySelector('.header-menu');
+const nav = document.querySelector('nav');
+
+userEvents.forEach((userEvent) => {
+  mobileBtn.addEventListener(userEvent, (e) => {
+    if (e.type === 'touchstart') e.preventDefault();
+    nav.classList.add('active-hamburger');
+    outsideClick(menuList, userEvents, () => {
+      nav.classList.remove('active-hamburger');
+    });
+
+    const active = nav.classList.contains('active-hamburger');
+    // Will set true or false
+    e.currentTarget.setAttribute('aria-expanded', active);
+
+    if (active) e.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    else e.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  });
+});
